3b9fd3b03f4766c20a4095551e0babe6ad9961ef
JakubBednarz <j_bednarz28.03.92@wp.pl>
2020-05-03 08:48:50 +0200

    Updates

---
+++ precache-manifest.03689a1d3d39032214e0a6cd787a7a87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772abd0676743ea4cbd9546f7d09a04f",
+    "url": "/Modal/index.html"
+  },
+  {
+    "revision": "e2892e303870979847c4",
+    "url": "/Modal/static/js/2.8154f61f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Modal/static/js/2.8154f61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c202fb5189d0cad8bca9",
+    "url": "/Modal/static/js/main.77cdacf6.chunk.js"
+  },
+  {
+    "revision": "639a429dbd9ccd058651",
+    "url": "/Modal/static/js/runtime-main.60fd685a.js"
+  }
+]);
